b8eac0b53d35ee18683cdd385a2419342bde465b
Eduardo Zumbado Granados <42503164+eduardo2108@users.noreply.github.com>
2020-06-03 00:57:12 -0600

    Implemented Duel event

---
+++ src/Proyecto1/DatosParty/Minigames/RockPaperScissors.java
@@ -0,0 +1,11 @@
+package Proyecto1.DatosParty.Minigames;
+
+import Proyecto1.DatosParty.DataStructures.SimpleLinkedList.SimpleLinkedList;
+import Proyecto1.DatosParty.Player;
+
+public class RockPaperScissors extends Minigame {
+    @Override
+    public void play(SimpleLinkedList<Player> players) {
+
+    }
+}
